c74d39327558bd92d125b2505d8bbf763462d5df
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-06-07 18:41:46 +0200

    Update spoolman_service.py

--- spoolman_service.py
+++ spoolman_service.py
@@ -104,10 +104,12 @@ def spendFilaments(printdata):
   
   ams_usage = []
   print(printdata)
+  filamentOrder = printdata["filamentOrder"]
+  filament_id_to_amstray = {fid: tray for tray, fid in filamentOrder.items()}
   for filamentId, filament in printdata["filaments"].items():
     if ams_mapping[0] != EXTERNAL_SPOOL_ID:
         try:
-            tray_id = ams_mapping[filamentId - 1]
+            tray_id = filament_id_to_amstray.get(filamentId - 1)
             #tray_id = ams_mapping.index(filamentId - 1)   # get tray_id from ams_mapping for filament
             ams_id = getAMSFromTray(tray_id)        # caclulate ams_id from tray_id
             tray_id = tray_id - ams_id * 4          # correct tray_id for ams
