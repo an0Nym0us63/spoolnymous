cc27043250dc306d2aef2d46919b7f9b0734444d
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-08-06 18:16:39 +0200

    Update mqtt_bambulab.py

--- mqtt_bambulab.py
+++ mqtt_bambulab.py
@@ -8,14 +8,14 @@ import paho.mqtt.client as mqtt
 
 from config import PRINTER_ID, PRINTER_CODE, PRINTER_IP, AUTO_SPEND, EXTERNAL_SPOOL_AMS_ID, EXTERNAL_SPOOL_ID
 from messages import GET_VERSION, PUSH_ALL
-from spoolman_service import spendFilaments, setActiveTray, fetchSpools,clearActiveTray,delete_tray_spool_map_by_id
+from spoolman_service import spendFilaments, setActiveTray, fetchSpools,clearActiveTray
 from tools_3mf import getMetaDataFrom3mf
 import time
 import copy
 import math
 from collections.abc import Mapping
 from logger import append_to_rotating_file
-from print_history import  insert_print, insert_filament_usage, update_filament_spool,update_print_status_with_job_id,get_tray_spool_map
+from print_history import  insert_print, insert_filament_usage, update_filament_spool,update_print_status_with_job_id,get_tray_spool_map,delete_tray_spool_map_by_id
 
 from globals import PRINTER_STATUS, PRINTER_STATUS_LOCK, PROCESSED_JOBS, PENDING_JOBS
 
