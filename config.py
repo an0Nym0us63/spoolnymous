489e2288970d36c7e9f70d8c9a8a28a63bd7cfed
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-08-07 11:16:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,72 @@
 import os
+import sqlite3
+
 DEFAULT_ADMIN_USERNAME = "admin"
 DEFAULT_ADMIN_PASSWORD = "admin"
 EXTERNAL_SPOOL_AMS_ID = 255 # don't change
 EXTERNAL_SPOOL_ID = 254 #  don't change
-
-BASE_URL = os.getenv('OPENSPOOLMAN_BASE_URL') # Where will this app be accessible
-PRINTER_ID = os.getenv('PRINTER_ID')  # Printer serial number - Run init_bambulab.py
-PRINTER_CODE = os.getenv('PRINTER_ACCESS_CODE')       # Printer access code - Run init_bambulab.py
-PRINTER_IP = os.getenv('PRINTER_IP')     # Printer local IP address - Check wireless on printer
-PRINTER_NAME = os.getenv('PRINTER_NAME')     # Printer name - Check wireless on printer
-SPOOLMAN_BASE_URL = os.getenv('SPOOLMAN_BASE_URL')
-SPOOLMAN_API_URL = f"{SPOOLMAN_BASE_URL}/api/v1"
-AUTO_SPEND = os.getenv('AUTO_SPEND', False)
+AUTO_SPEND = True
 SPOOL_SORTING = os.getenv('SPOOL_SORTING', "filament.material:asc,filament.vendor.name:asc,filament.name:asc")
-LOCATION_MAPPING = os.getenv('LOCATION_MAPPING','')
-AMS_ORDER = os.getenv('AMS_ORDER','')
-COST_BY_HOUR = os.getenv('COST_BY_HOUR',0.042)
+PRINTER_NAME=""
+
+BASE_URL = get_app_setting("OPENSPOOLMAN_BASE_URL", use_env=True)
+PRINTER_ID = get_app_setting("PRINTER_ID", use_env=True)
+PRINTER_CODE = get_app_setting("PRINTER_ACCESS_CODE", use_env=True)
+PRINTER_IP = get_app_setting("PRINTER_IP", use_env=True)
+SPOOLMAN_BASE_URL = get_app_setting("SPOOLMAN_BASE_URL", use_env=True)
+SPOOLMAN_API_URL = f"{SPOOLMAN_BASE_URL}/api/v1"
+LOCATION_MAPPING = get_app_setting("LOCATION_MAPPING",default='', use_env=True)
+AMS_ORDER = get_app_setting("AMS_ORDER",default='',use_env=True)
+COST_BY_HOUR = get_app_setting("COST_BY_HOUR",default=0,use_env=True)
+
+db_config = {"db_path": os.path.join(os.getcwd(), 'data', "3d_printer_logs.db")}
+
+def init_settings_table():
+    conn = sqlite3.connect(db_config["db_path"])
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS settings (
+            key TEXT PRIMARY KEY,
+            value TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+app_settings_cache = None
+
+def load_app_settings_cache(force_reload=False):
+    global app_settings_cache
+    if app_settings_cache is None or force_reload:
+        conn = sqlite3.connect(db_config["db_path"])
+        cursor = conn.cursor()
+        cursor.execute("SELECT key, value FROM settings")
+        app_settings_cache = dict(cursor.fetchall())
+        conn.close()
+
+def get_app_setting(key: str, default=None, use_env=True) -> str | None:
+    load_app_settings_cache()
+    if key in app_settings_cache:
+        return app_settings_cache[key]
+    if use_env:
+        return os.getenv(key, default)
+    return default
+
+def set_app_setting(key: str, value: str) -> None:
+    global app_settings_cache
+    conn = sqlite3.connect(db_config["db_path"])
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO settings (key, value)
+        VALUES (?, ?)
+        ON CONFLICT(key) DO UPDATE SET value = excluded.value
+    """, (key, value))
+    conn.commit()
+    conn.close()
+    app_settings_cache = None  # Invalider le cache
+
+def get_all_app_settings() -> dict:
+    load_app_settings_cache()
+    return app_settings_cache.copy()
+
+init_settings_table()
