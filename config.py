dcfb88e821a0fa3d086b08732ddd32b6a68a8af9
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-08-07 11:17:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,6 @@
 import os
 import sqlite3
 
-DEFAULT_ADMIN_USERNAME = "admin"
-DEFAULT_ADMIN_PASSWORD = "admin"
-EXTERNAL_SPOOL_AMS_ID = 255 # don't change
-EXTERNAL_SPOOL_ID = 254 #  don't change
-AUTO_SPEND = True
-SPOOL_SORTING = os.getenv('SPOOL_SORTING', "filament.material:asc,filament.vendor.name:asc,filament.name:asc")
-PRINTER_NAME=""
-
-BASE_URL = get_app_setting("OPENSPOOLMAN_BASE_URL", use_env=True)
-PRINTER_ID = get_app_setting("PRINTER_ID", use_env=True)
-PRINTER_CODE = get_app_setting("PRINTER_ACCESS_CODE", use_env=True)
-PRINTER_IP = get_app_setting("PRINTER_IP", use_env=True)
-SPOOLMAN_BASE_URL = get_app_setting("SPOOLMAN_BASE_URL", use_env=True)
-SPOOLMAN_API_URL = f"{SPOOLMAN_BASE_URL}/api/v1"
-LOCATION_MAPPING = get_app_setting("LOCATION_MAPPING",default='', use_env=True)
-AMS_ORDER = get_app_setting("AMS_ORDER",default='',use_env=True)
-COST_BY_HOUR = get_app_setting("COST_BY_HOUR",default=0,use_env=True)
-
 db_config = {"db_path": os.path.join(os.getcwd(), 'data', "3d_printer_logs.db")}
 
 def init_settings_table():
@@ -70,3 +52,21 @@ def get_all_app_settings() -> dict:
     return app_settings_cache.copy()
 
 init_settings_table()
+
+DEFAULT_ADMIN_USERNAME = "admin"
+DEFAULT_ADMIN_PASSWORD = "admin"
+EXTERNAL_SPOOL_AMS_ID = 255 # don't change
+EXTERNAL_SPOOL_ID = 254 #  don't change
+AUTO_SPEND = True
+SPOOL_SORTING = os.getenv('SPOOL_SORTING', "filament.material:asc,filament.vendor.name:asc,filament.name:asc")
+PRINTER_NAME=""
+
+BASE_URL = get_app_setting("OPENSPOOLMAN_BASE_URL", use_env=True)
+PRINTER_ID = get_app_setting("PRINTER_ID", use_env=True)
+PRINTER_CODE = get_app_setting("PRINTER_ACCESS_CODE", use_env=True)
+PRINTER_IP = get_app_setting("PRINTER_IP", use_env=True)
+SPOOLMAN_BASE_URL = get_app_setting("SPOOLMAN_BASE_URL", use_env=True)
+SPOOLMAN_API_URL = f"{SPOOLMAN_BASE_URL}/api/v1"
+LOCATION_MAPPING = get_app_setting("LOCATION_MAPPING",default='', use_env=True)
+AMS_ORDER = get_app_setting("AMS_ORDER",default='',use_env=True)
+COST_BY_HOUR = get_app_setting("COST_BY_HOUR",default=0,use_env=True)
