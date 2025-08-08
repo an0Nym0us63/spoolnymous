895cb2cc89b28206e1b1576b00a78f210a706474
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-08-08 18:37:47 +0200

    Revert "fix"
    This reverts commit e0cdb40e6ed9beb47fd4f46c71dd698050503c5b.

--- static/js/print_history.js
+++ static/js/print_history.js
@@ -112,8 +112,7 @@ $(document).ready(function () {
 					if ($.trim(params.term) === '') return data;
 					if (typeof data.text === 'undefined') return null;
 					return data.text.toLowerCase().includes(params.term.toLowerCase()) ? data : null;
-				},
-				dropdownCssClass: "select2-search-visible"
+				}
 			});
 		}
 
