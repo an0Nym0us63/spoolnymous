91ea51a8ebb5dea0638cb631ca9ff8171f9ea118
sarakha63 <sarakha_ludovic@yahoo.fr>
2025-09-20 17:07:24 +0200

    sss

--- filaments.py
+++ filaments.py
@@ -2161,6 +2161,8 @@ def augmentTrayData(spool_list, tray_data, tray_id):
 
     # S'il y a une config (type) mais qu'on n'a rien trouvé: flag issue
     tray_data["issue"] = bool(tray_data.get("tray_type")) and not tray_data["matched"]
+    if tray_data["tray_uuid"] != "00000000000000000000000000000000" and "foundMode" in tray_data and tray_data["foundMode"] != "Tag":
+        tray_data["issue"] = True
 
     # Si non matché -> purger l'affichage pour éviter les reliquat
     #if not tray_data["matched"]:
